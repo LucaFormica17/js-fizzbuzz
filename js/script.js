baebe721807fa8731359173327408a75e88f5195
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-11-23 10:04:42 +0100

    [correzzione]

--- js/script.js
+++ js/script.js
@@ -1,29 +1,26 @@
+//BONUS
+const list = document.getElementById('ul');
+
 //MOSTRARE in console numeri da 1 a 100
 for(let i=1; i<101; i++){
+    let see;
     //CONDIZIONARE LA VISUALIZZAZIONE DEI NUMERI MULTIPLI DI 3, 5 e 3 && 5
     if(i%3 == 0 && i%5 == 0){
-        console.log(i+'fizzbuzz');
+        see = `<li>fizzbuzz</li>`;
+        console.log('fizzbuzz');
     }
-    else if(i&5 == 0){
-        console.log(i+'fizz');
+    else if(i%5 == 0){
+        see = `<li>fizz</li>`;
+        console.log('fizz');
     }
     else if(i%3 == 0){
-        console.log(i+'buzz');
+        see = `<li>buzz</li>`;
+        console.log('buzz');
     }
     else{
-        
+        see = `<li>${i}</li>`;
         console.log(i);
     }
+    list.innerHTML += see;
 }
 
-
-
-//BONUS
-const list = document.querySelector('ul');
-for(i=1; 1<101; i++){
-let li = document.createElement('li'); //createelement crea un elemento html)
-    li.setAttribute('id', `list-item-${i}`);
-    li.innerHTML = `Voce lista ${i}`;
-    list.appendChild(li);
-    console.log(i);
-}
